90bf864392d422b5fc48536ce2a6e72a6766ffe8
wii <zhenkai.sun@qq.com>
2021-01-19 21:38:47 +0800

    reorgnize code

--- git-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# echo `pwd` > /home/bovenson/Tmp/cron.log
-
-FULLPATH="$PWD/$(dirname $0)"
-cd $FULLPATH
-
-git add --all
-git commit -m "auto commit"
-git push origin master
--- run-project.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/python3 manage.py runserver 0.0.0.0:9002 2>/dev/null 1>/dev/null &
