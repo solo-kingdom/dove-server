6cfde5aae5487965d959da691aba53c906f8d16a
wii <zhenkai.sun@qq.com>
2021-01-27 14:35:41 +0000

    add start & stop

--- databases/tidb/deploy-cluster.sh
+++ databases/tidb/deploy-cluster.sh
@@ -1,1 +1,1 @@
-tiup cluster deploy tidb-cloud-library v4.0.0 ./tidb-cluster.yaml --user root -p xkss9655
+tiup cluster deploy cloud-library v4.0.0 ./config/tidb-cluster.yaml --user root -p xkss9655
